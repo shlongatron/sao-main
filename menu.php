fbab1047aca1c9aefdee49dd8df123ca7112c8c6
shlongatron <maciekradziejowski@gmail.com>
2022-05-01 21:54:29 +0200

    sex

---
+++ menu.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <link rel="stylesheet" href="menu.css">
+</head>
+<body>
+    <nav>
+        <ul>
+            <li><a class="active" href="#home">Strona główna</a></li>
+            <li class="right"><a href="#contact">Zaloguj się</a></li>
+            <!-- siemano kocury !-->
+        </ul>
+    </nav>
+</body>
+</html>
